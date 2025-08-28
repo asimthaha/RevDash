ddd84b073b245158cf17535b6cdeaa5e2fe0007e
Asim Thaha <iamasimthaha@gmail.com>
2025-08-28 20:14:31 +0530

    added basepath

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/RevDash">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/revenue" element={<Revenue />} />
